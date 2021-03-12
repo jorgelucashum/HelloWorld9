118fa4621199abb7fb86005c2cbb3838204ca491
jorgeucas lucas <jorge_dofus1@hotmail.com>
2021-03-12 15:26:20 -0300

    Add project files.

---
+++ HelloWorld9/Calculadora.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld9
+{
+    class Calculadora
+    {
+        // static:
+        public static double Pi = 3.14; // ao se usar static a classe não precisa ser instanciada por um objeto, dessa forma tendo um valor estático/fixo toda vez que for chamada
+
+        public static double Circunferencia(double r)
+        {
+            return 2.0 * Pi * r;
+        }
+
+        public static double Volume(double r)
+        {
+            return 4.0 / 3 * Pi * r * r * r;
+        } 
+    }
+}
---
+++ HelloWorld9/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Digite o valor do Raio");
+            double raio = double.Parse(Console.ReadLine());
+
+            double circ = Calculadora.Circunferencia(raio); // Como os métodos são estáticos, não precisa de instancia, podendo ser chamado direto pela classe 'Calculadora.'
+            double volume = Calculadora.Volume(raio);
+
+            Console.WriteLine("Circunferencia: " + circ.ToString("F2") + "\nVolume: " + volume.ToString("F2") + "\nPi: " + Calculadora.Pi);
+
+            Console.ReadLine();
+
+        }
+    }
+}
